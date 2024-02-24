2ced9a1a824102d4bf07817891b9dfee4c1c26ef
hyeuoo <161108761+hyeuoo@users.noreply.github.com>
2024-02-24 15:14:21 +0900

    Add files via upload

---
+++ hello_test.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+
+/*
+int main()
+{
+    printf("Hello world\n");
+ 
+    return 0;
+}
+
+int main(){
+    printf("삼성디스플레이 \n");
+    printf("대형 라인전환 T/F \n");
+    printf("051241105 김혜지 \n");
+     return 0;
+}
+
+
+int main(){
+    printf("  @ @ \n");
+    printf("(=^.^=) \n");
+    printf(" -m-m- \n");
+     return 0;
+}
+
+int main() {
+    printf("삼성디스플레이 \n 대형 라인전환 T/F \n 051241105 김혜지 \n");
+    return 0;
+}
+
+int main() {
+    printf("CCCCCCCCCC\n");
+    printf("C        C\n");
+    printf("C        C\n");
+    printf("C        C\n");
+    printf("C        C\n");
+    printf("CCCCCCCCCC\n");
+    return 0;
+}
+
+
+int main() {
+    printf(" CCCCCCCCCC\n C        C\n C        C\n C        C\n C        C\n CCCCCCCCCC\n");
+    return 0;
+}
+
+
+int main() {
+    printf("첫 번째 숫자 : %d\n",123);
+    printf("두 번째 숫자 : %.2f\n",7.6);
+
+    return 0;
+} 
+
+int main() {
+    printf("실수 입력 : %f\n",7.4534);
+    printf("실수 입력 : %.3f\n",7.4534);
+    printf("실수 입력 : %.2f\n",7.4534);
+    printf("실수 입력 : %.1f\n",7.4534);
+    printf("실수 입력 : %.0f\n",7.4534);
+
+    return 0;
+}
+
+int main() {
+    printf("실수 입력 : %10d\n",1234);
+    printf("실수 입력 : %8d\n",1234);
+    printf("실수 입력 : %9d\n",1234);
+    printf("실수 입력 : %7d\n",1234);
+    printf("실수 입력 : %2d\n",1234);
+
+    return 0;
+}*/
+
+// int main(){
+//     printf("%s\n","각 과목의 점수와 합계 , 평균을 출력한다.");
+//     printf("kor %d , mat %d , eng %d\n", 55,65,90);
+//     printf("sum %d\n",55+65+90);
+//     printf("avg %d\n",(55+65+90)/3);
+//     return 0;
+// }
+
+
+// int main(){
+
+//     int sum = 55+65+90;
+
+//     printf("%s\n","각 과목의 점수와 합계 , 평균을 출력한다.");
+//     printf("kor %d , mat %d , eng %d\n", 55,65,90);
+//     printf("sum %d\n",sum);
+//     printf("avg %d\n",sum/3);
+//     return 0;
+// }
+
+// int main(){
+
+//     int kor,mat,eng ,sum,avg ;
+
+//      printf("국어 점수를 입력하세요: ");
+//     scanf("%d", &kor);
+
+//     printf("수학 점수를 입력하세요: ");
+//     scanf("%d", &mat);
+
+//     printf("영어 점수를 입력하세요: ");
+//     scanf("%d", &eng);
+
+//     sum = kor + mat + eng ;
+//     avg = sum / 3;
+
+//     printf("국어: %d, 수학: %d, 영어: %d\n", kor, mat, eng);
+//     printf("총점: %d\n", sum);
+//     printf("평균: %d\n", avg);
+
+//     return 0;
+// }
+
+
+// int main(){
+//    printf("%s%8s\n", "sub", "score");
+//     printf("--------------\n");
+//     printf("%s%8d\n", "kor", 50);
+//     printf("%s%8d\n", "mat", 65);
+//     printf("%s%8d\n", "eng", 90);
+//     printf("--------------\n");
+//     printf("%s%8d\n", "sum", 50+65+90);
+//     printf("%s%8.2lf\n", "avg", (50+65+90)/3);
+//     return 0;
+// }
+
+
+// int main() {
+//     int a = 2;
+//     char b;
+//     b = 'c';
+    
+//     printf("a = %d\nb = %c\n", a, b);
+    
+//     return 0;
+// }
+
+
+// int main() {
+//     printf("Fun Programming!");
+
+//     return 0; 
+// }
+
+// int main() {
+//     printf("두번째 프로그램입니다.");
+//     printf("한 줄에 여러 문장을 출력합니다.");
+//     return 0;
+//  }
+
+
+// int main() {
+//     printf("세번째 프로그램입니다.\n");
+//     printf("줄을 바꾸어 출력합니다.\n");
+//     printf("여기까지 출력하고\n줄을 바꿉니다.");
+//     return 0;
+//  }
+
+
+// int main() {
+//     printf("%s","My name is Hye Ji Kim.\n I am 37 years old.");
+//     return 0;
+//  }
+
+
+// int main(){
+//     printf("  @@  \n");
+//     printf(" @  @ \n");
+//     printf("@    @\n");
+//     printf(" @  @ \n");
+//     printf("  @@  \n");
+//     return 0;
+// }
+
+// int main() {
+//     printf("My name is %s. I am %d years old.\n", "Hye Ji Kim", 37);
+
+//     return 0;
+// }
+
+
+// int main(){
+//     printf("서식 문자로 출력하는 방법입니다.\n");
+//     printf("%s","이것은 문자열을 나타냅니다\n");
+    
+//     printf("수를 출력할 때는 따옴표를 생략합니다.\n");
+//     printf("%d\n",123);
+//     printf("%.1f\n",6.5);
+//     return 0;
+// }
+
+
+// int main(){
+//     printf("%15s%15s%15s\n", "Seoul", "10,312,545","+31,375");
+//     printf("%15s%15s%15s\n", "Pusan", "3,567,910","+5,868");
+//     return 0;
+//  }
+
+int main (){
+
+    printf("My height\n");
+    printf("%d\n",172);
+    printf("My weight\n");
+    printf("%d\n",78);
+    
+    return 0;
+
+}
